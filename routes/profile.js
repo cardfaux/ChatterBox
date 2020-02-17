cf1cb3e8892549c4e2575175a16816ec69c644fe
James Hagood <cardfaux@tutanota.com>
2020-02-16 19:43:23 -0600

    added get route to /api/profile/me to get current users profile

--- routes/profile.js
+++ routes/profile.js
@@ -1,9 +1,28 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../middleware/auth');
 
-// @route  GET api/profile
-// @desc   TEST route
-// @access Public
-router.get('/', (req, res) => res.send('Profile Route'));
+const Profile = require('../models/Profile');
+const User = require('../models/User');
+
+// @route  GET api/profile/me
+// @desc   Get Current Users Profile
+// @access Private
+router.get('/me', auth, async (req, res) => {
+	try {
+		const profile = await Profile.findOne({
+			user: req.user.id
+		}).populate('user', ['name', 'avatar', 'image']);
+
+		if (!profile) {
+			return res.status(400).json({ msg: 'There Is No Profile For This User' });
+		}
+
+		res.json(profile);
+	} catch (error) {
+		console.error(error.message);
+		res.status(500).send('Server Error');
+	}
+});
 
 module.exports = router;
